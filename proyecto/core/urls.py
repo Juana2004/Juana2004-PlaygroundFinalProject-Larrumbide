e2205c5859142ee4a80913bc2bf808305bc2cdf3
Juana Larrumbide <juanalarrumbide@gmail.com>
2024-02-04 14:18:59 -0500

    end

---
+++ proyecto/core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("persona/create", views.persona_create, name="persona_create"),
+]
---
+++ proyecto/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from . import models
+from . import forms 
+
+def index(request):
+    return render(request, "core/index.html")
+
+def persona_create(request):
+    if request.method == "POST":
+        form = forms.PersonaForm(request.POST)
+        if form.is_valid():
+            form.save() 
+    else:
+        form = forms.PersonaForm()
+    return render(request, "core/persona_create.html", {"form" : form })
